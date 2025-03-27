18b7a6d093948bbceafd439cd6c8ac27569ebc41
sattarov <vix123456@gmail.com>
2025-03-27 21:54:15 +0400

    version com

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -1,30 +1,25 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 using System.Windows;
 
 namespace WpfApp
 {
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
-        private ObservableCollection<Company> _companies;
-
-        public ObservableCollection<Company> Companies
-        {
-            get { return _companies; }
-            set { _companies = value; OnPropertyChanged("Companies"); }
-        }
+        public ObservableCollection<Company> Companies { get; set; }
 
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = this;
             Companies = new ObservableCollection<Company>
             {
-                new Company { Name = "", ProductCount = 0, DiscountLevel = DiscountType.Bronze },
-                new Company { Name = "", ProductCount = 0, DiscountLevel = DiscountType.Bronze },
-                new Company { Name = "", ProductCount = 0, DiscountLevel = DiscountType.Bronze }
+                new Company(),
+                new Company(),
+                new Company()
             };
+            DataContext = this;
         }
 
         private void CalculateDiscounts(object sender, RoutedEventArgs e)
@@ -34,72 +29,67 @@ namespace WpfApp
                 company.CalculateDiscount();
             }
         }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-    }
-
-    public enum DiscountType
-    {
-        Bronze = 5,
-        Silver = 10,
-        Gold = 15
-    }
-
-    public static class DiscountTypeHelper
-    {
-        public static Array Values => Enum.GetValues(typeof(DiscountType));
     }
 
     public class Company : INotifyPropertyChanged
     {
         private string _name;
         private int _productCount;
-        private DiscountType _discountLevel;
+        private bool _isBronze, _isSilver, _isGold;
         private double _totalDiscount;
 
         public string Name
         {
-            get { return _name; }
-            set { _name = value; OnPropertyChanged("Name"); }
+            get => _name;
+            set { _name = value; OnPropertyChanged(nameof(Name)); }
         }
 
         public int ProductCount
         {
-            get { return _productCount; }
-            set { _productCount = value; OnPropertyChanged("ProductCount"); }
+            get => _productCount;
+            set { _productCount = value; OnPropertyChanged(nameof(ProductCount)); }
         }
 
-        public DiscountType DiscountLevel
+        public bool IsBronze
         {
-            get { return _discountLevel; }
-            set { _discountLevel = value; OnPropertyChanged("DiscountLevel"); }
+            get => _isBronze;
+            set { _isBronze = value; OnPropertyChanged(nameof(IsBronze)); }
+        }
+
+        public bool IsSilver
+        {
+            get => _isSilver;
+            set { _isSilver = value; OnPropertyChanged(nameof(IsSilver)); }
+        }
+
+        public bool IsGold
+        {
+            get => _isGold;
+            set { _isGold = value; OnPropertyChanged(nameof(IsGold)); }
         }
 
         public double TotalDiscount
         {
-            get { return _totalDiscount; }
-            set { _totalDiscount = value; OnPropertyChanged("TotalDiscount"); }
+            get => _totalDiscount;
+            set { _totalDiscount = value; OnPropertyChanged(nameof(TotalDiscount)); }
         }
 
         public void CalculateDiscount()
         {
-            double baseDiscount = (double)DiscountLevel;
-            if (ProductCount >= 10000 && ProductCount < 50000) baseDiscount += 5;
-            else if (ProductCount >= 50000 && ProductCount < 300000) baseDiscount += 10;
-            else if (ProductCount >= 300000 && ProductCount < 1000000000) baseDiscount += 15;
+            double levelDiscount = IsBronze ? 5 : IsSilver ? 10 : IsGold ? 15 : 0;
+            double productDiscount = ProductCount switch
+            {
+                >= 10000 and < 50000 => 5,
+                >= 50000 and < 300000 => 10,
+                >= 300000 and < 1000000000 => 15,
+                _ => 0
+            };
 
-            TotalDiscount = baseDiscount;
+            TotalDiscount = levelDiscount + productDiscount;
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged(string propertyName)
-        {
+        protected void OnPropertyChanged(string propertyName) =>
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
     }
 }
-
